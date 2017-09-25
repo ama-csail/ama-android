58af691c75d83e423f5d62437deb442d3b30608d
vontell <vontell@mit.edu>
2017-09-24 23:19:15 -0400

    Created general local storage interface

--- ama/src/main/java/io/github/ama_csail/ama/util/storage/SystemConfig.java
+++ ama/src/main/java/io/github/ama_csail/ama/util/storage/SystemConfig.java
@@ -6,4 +6,25 @@ package io.github.ama_csail.ama.util.storage;
  * @author Aaron Vontell
  */
 public class SystemConfig {
+
+    private static final String VERSION = "0.0.1";
+    private static final String HOMEPAGE = "http://ama-csail.github.io";
+
+
+    /**
+     * Returns a string representing the version of this AMA library.
+     * @return a string representing the version of this AMA library.
+     */
+    public static String getVersion() {
+        return VERSION;
+    }
+
+    /**
+     * Returns a url to a page which can be visited for more information about the library.
+     * @return a url to a page which can be visited for more information about the library.
+     */
+    public static String getHomepage() {
+        return HOMEPAGE;
+    }
+
 }
