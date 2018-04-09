400ef098811cdb22a097ab17ec9a896c97a224d7
vontell <vontell@mit.edu>
2018-04-09 17:34:31 -0400

    Blank, inaccessible app

--- app/src/main/java/io/github/ama_csail/amaexampleapp/MainActivity.java
+++ app/src/main/java/io/github/ama_csail/amaexampleapp/MainActivity.java
@@ -1,33 +1,28 @@
 package io.github.ama_csail.amaexampleapp;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.customtabs.CustomTabsIntent;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-import io.github.ama_csail.ama.AccessibleActivity;
-import io.github.ama_csail.ama.menu.OnAccessibleMenuConnectedListener;
-import io.github.ama_csail.ama.menu.OnInstructionsLoadedListener;
 import io.github.ama_csail.amaexampleapp.news.Article;
 import io.github.ama_csail.amaexampleapp.news.NewsApi;
 
-public class MainActivity extends AccessibleActivity {
+public class MainActivity extends AppCompatActivity {
 
     private LinearLayout newsLayout;
     private Activity activity;
@@ -37,72 +32,22 @@ public class MainActivity extends AccessibleActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        newsLayout = (LinearLayout) findViewById(R.id.news_layout);
-
+        // Loads the layout for news item
+        newsLayout = findViewById(R.id.news_layout);
         new NewsApiTask().execute(this);
 
+        // Save an instance of the context for news loading capabilities
         activity = this;
 
-        String test = "this is a lint test";
-
-
-    }
-
-    protected void configureAccessibility() {
-
-        //enableMenu();
-        //enableDyslexiaFont(true);
-        //AMA.increaseFontSize(getRootView(), 30);
-        //AMA.setActionClass(newsLayout, ActionClass.DANGER);
-        //Log.e("ACTION", AMA.getActionClass(newsLayout).toString());
-
-        setOnAccessibleMenuConnectedListener(new OnAccessibleMenuConnectedListener() {
-            @Override
-            public void configureMenu() {
-
-                // Create a glossary relevant to the information on the page
-                provideGlossary(AccessibleDefinitions.getGlossary());
-
-                // Create a process for loading instructions to be displayed to the user
-                setOnInstructionsLoadedListener(null, new OnInstructionsLoadedListener() {
-                    @Override
-                    public void onInstructionsLoaded(ViewGroup parent, Object config) {
-                        parent.removeAllViews();
-                        LayoutInflater.from(activity)
-                                .inflate(R.layout.main_instruction_layout, parent);
-                    }
-                });
-
-                enableLanguageSettings();
-            }
-        });
-
     }
 
     /**
-     * A collection of static variables which are used as configurations in the accessibility
-     * process
+     * Starts the process of downloading data from the News API
      */
-    public static class AccessibleDefinitions {
-
-        public static Map<String, String> getGlossary() {
-            Map<String, String> terms = new HashMap<>();
-            terms.put("Accessible", "To be usable in different contexts, no matter the operator");
-            terms.put("Glossary", "A list of terms and definitions");
-            terms.put("Ars Technica", "A news source with articles about computer, technology, start-ups, and more.");
-            terms.put("MIT", "An institute in Cambridge, MA which is renowned for it's incredible work in mobile accessibility, among other topics.");
-            terms.put("AMA", "A project created to address the issues with mobile accessibility.");
-            terms.put("Term", "This is just another term that I came up with");
-            return terms;
-        }
-
-    }
-
-    /**
-     * Starts the process of downloading data from the New API
-     */
+    @SuppressLint("StaticFieldLeak")
     private class NewsApiTask extends AsyncTask<Context, Void, Void> {
 
+        // Headlines is a list of headline articles from Ars Technica
         List<Article> headlines;
         Context context;
 
@@ -114,6 +59,7 @@ public class MainActivity extends AccessibleActivity {
         @Override
         protected Void doInBackground(Context... params) {
 
+            // Grab information from the news API
             context = params[0];
             headlines = NewsApi.getArsNews();
             return null;
@@ -122,6 +68,8 @@ public class MainActivity extends AccessibleActivity {
         @Override
         protected void onPostExecute(Void aVoid) {
             super.onPostExecute(aVoid);
+
+            // First, clear the news view of any existing articles
             newsLayout.removeAllViews();
 
             LayoutInflater inflater = (LayoutInflater) context
@@ -129,38 +77,41 @@ public class MainActivity extends AccessibleActivity {
 
             for (Article a : headlines) {
 
+                // For each article, create a card to display information
+
+                // This is the overall container for the card
                 View cardContainer = inflater.inflate(R.layout.news_headline, null);
+
+                // Add text content for the article title and author
                 String byLine = "Written by " + a.getAuthor();
                 ((TextView) cardContainer.findViewById(R.id.headline_title)).setText(a.getTitle());
                 ((TextView) cardContainer.findViewById(R.id.headline_subline)).setText(byLine);
 
+                // Load an image into an ImageView within this card container
                 Picasso.with(context)
                         .load(a.getUrlToImage())
                         .into((ImageView) cardContainer.findViewById(R.id.headline_image));
 
+                // For each article cards, set a click listener to open the web page
+                // NOTE: You shouldn't have to touch this code for the purpose of this project
                 final Article finalArticle = a;
                 cardContainer.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
+
                         Uri uri = Uri.parse(finalArticle.getUrl());
-
-                        // create an intent builder
                         CustomTabsIntent.Builder intentBuilder = new CustomTabsIntent.Builder();
-
                         intentBuilder.setToolbarColor(ContextCompat.getColor(context, R.color.colorPrimary));
                         intentBuilder.setSecondaryToolbarColor(ContextCompat.getColor(context, R.color.colorPrimaryDark));
-
                         CustomTabsIntent customTabsIntent = intentBuilder.build();
-
                         customTabsIntent.launchUrl(activity, uri);
 
                     }
                 });
 
+                // Finally, add this new article information to the news layout
                 newsLayout.addView(cardContainer);
             }
-
-            configureAccessibility();
 
         }
 
